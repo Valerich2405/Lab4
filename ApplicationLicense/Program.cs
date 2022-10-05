298a6528a260e6863f140f8beb8a7f42123a67e9
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2022-10-05 13:51:53 +0300

    Add ApplicationLicense

---
+++ ApplicationLicense/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ApplicationLicense
+{
+    public class ApplicationLicense
+    {
+        public void AllowCommon()
+        {
+            Console.WriteLine("---Free version---");
+        }
+
+        public void AllowTrial()
+        {
+            Console.WriteLine("---Trial version---");
+        }
+
+        public void AllowPro()
+        {
+            Console.WriteLine("---Pro version---");
+        }
+    }
+
+    public class Check : ApplicationLicense
+    {
+        private int password;
+        public Check(int Password)
+        {
+            this.password = Password;
+        }  
+
+        public void CheckApplication()
+        {
+            if(password == 12345)
+            {
+                AllowTrial();
+            }
+
+            else if(password == 123456789)
+            {
+                AllowPro();
+            }
+
+            else
+            {
+                AllowCommon();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your password: ");
+            int password = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Check check = new Check(password);
+
+            check.CheckApplication();
+        }
+    }
+}
